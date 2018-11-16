7b4f8c83f3527237ab75dbb6197683fca7f2f53b
jaylan <jaylanchen@126.com>
2018-11-16 12:51:15 +0800

    remove old code

--- MiArea/Models/TownModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiArea.Models
-{
-    public class TownModel
-    {
-        public int Region_id { get; set; }
-        public string Region_name { get; set; }
-        public string ZipCode { get; set; }
-    }
-}
